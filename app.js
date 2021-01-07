96dfa7e036f345ac8b3784630aacdeebe9c82e6f
Nikita Chanana <nikitachanana0106@gmail.com>
2021-01-07 19:07:42 +0530

    deploy

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
 const mainRoutes = require("./routes/MainRouter")
+const path = require("path")
 require('dotenv').config()
 const port = process.env.PORT || 5656
 const db = require("./config/database")
 app.use(express.json());
 app.use("/api", mainRoutes)
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static("client/build"));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client',"build",'index.html'))
+    })
+}
 
 app.listen(port, () => console.log(`App is running on  ${port}!`))
 // db.authenticate().then(() => {
